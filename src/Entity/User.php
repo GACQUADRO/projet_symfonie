5026560f8248936e200deb157e474dfeaf328ada
GACQUADRO <73235291+GACQUADRO@users.noreply.github.com>
2022-04-30 11:44:02 +0200

    make auth et register mais a completer

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -12,6 +12,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[UniqueEntity(fields: ['username'], message: 'There is already an account with this username')]
+#[UniqueEntity(fields: ['username'], message: 'There is already an account with this username')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
